80f5254b03ec717d2313e5d1259d545bab736a6c
brendanconnolly@gmail.com <brendanconnolly@gmail.com>
2018-03-29 15:53:48 -0700

    starting to add lambda support

---
+++ src/lambdaFun.js
@@ -0,0 +1,16 @@
+const gdax = require(`./gdaxClients.js`);
+const DepositFundsCommand = require(`./commands/depositFundsCommand.js`);
+const BuyCommand = require(`./commands/buyCommand.js`);
+
+const sandBoxMode = process.env.GDAX_USE_SANDBOX === 'true';
+const currencyCode = `USD`;
+
+const authdClient = gdax.authenticatedApi(sandBoxMode);
+
+exports.lambdaDepositHandler = (event, context) => {
+
+    let depositAmount = 100;
+    let depositCommand = new DepositFundsCommand(authdClient);
+    depositCommand.execute(depositAmount);
+
+};
