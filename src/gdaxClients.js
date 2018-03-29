a878e79197a00307ae64ff6270fd971f1b2689a2
brendanconnolly <brendanconnolly@gmail.com>
2018-03-29 00:09:58 -0700

    Update gdaxClients.js

--- src/gdaxClients.js
+++ src/gdaxClients.js
@@ -4,9 +4,9 @@ const gdaxApiUrl = (sandBoxMode) => sandBoxMode ? 'https://api-public.sandbox.gd
 
 
 //yeah these need to go
-const apiKey = `7d2a8d516afdb3b3eea90f02da32ed51`;
-const apiSecret = `MEO+hjZ750Wy9AbdwH3qLVYaDmPLzuBFC/YMFUJ7T6D7oCj0CrVNOpG4M2d8JB4I/TW+jUdkyyTtYuWPhyntdQ==`;
-const apiPassphrase = `sandyPass`;
+const apiKey = ``;
+const apiSecret = ``;
+const apiPassphrase = `xxx`;
 
 const publicApi = (sandBoxMode = true) => {
     let uri = gdaxApiUrl(sandBoxMode);
