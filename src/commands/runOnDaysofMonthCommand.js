56ecdce502cf877939ab6781104f7ad740f5b0f4
brendanconnolly@gmail.com <brendanconnolly@gmail.com>
2018-04-02 12:26:57 -0700

    updates to schedule

--- src/commands/runOnDaysofMonthCommand.js
+++ src/commands/runOnDaysofMonthCommand.js
@@ -1,12 +1,14 @@
 const ScheduleCommand = require(`./scheduledCommand.js`);
+var schedule = require('node-schedule');
 
-function RunOnDaysOfMonthCommand(commands, days, hourToFire = 3, minuteToFire = 0) {
+function RunOnDaysOfMonthCommand (commands, days, hourToFire = 3, minuteToFire = 0) {
     this.cmds = commands;
-    this.reoccurrenceRule = { date: [...days], minute: minuteToFire, hour: hourToFire };
+    //this.reoccurrenceRule = { date: [...days], minute: minuteToFire, hour: hourToFire };
+    this.reoccurrenceRule = { minute: new schedule.Range(0, 60) };
     this.counter = 0;
 };
 
-RunOnDaysOfMonthCommand.prototype.execute = async function () {
+RunOnDaysOfMonthCommand.prototype.execute = async function execute () {
 
     this.cmds.forEach((value, key) => {
         let schedule = new ScheduleCommand(key, value);
--- src/commands/scheduledCommand.js
+++ src/commands/scheduledCommand.js
@@ -1,14 +1,15 @@
 var schedule = require('node-schedule');
 
-function ScheduledCommand(jobName, command, reoccurrence) {
+function ScheduledCommand (jobName, command, reoccurrence) {
     this.cmd = command;
     this.name = jobName;
     this.reoccurrenceRule = reoccurrence;
     this.counter = 0;
 };
 
-ScheduledCommand.prototype.execute = async function () {
+ScheduledCommand.prototype.execute = async function execute () {
 
+    console.log(`running ${this.name}`);
     schedule.scheduleJob(this.name, this.reoccurrenceRule, this.cmd);
 };
 
